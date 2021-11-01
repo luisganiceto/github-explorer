c565dc4e9fbb1386a4783b305310bba7cd8292bf
Luis Aniceto <luisg.aniceto@hotmail.com>
2021-11-01 13:50:03 -0300

    Final

--- src/components/RepositotyList.tsx
+++ src/components/RepositotyList.tsx
@@ -3,8 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    id: number;
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositotyList(){
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect( () => {
         fetch('https://api.github.com/users/luisganiceto/repos')        
